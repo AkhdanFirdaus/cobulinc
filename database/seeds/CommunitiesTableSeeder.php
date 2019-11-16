696d7de345b8ac1fc2934bd2e9f8e8a3e7d5a549
Luthfi Sukmawardhani <luthfi552@gmail.com>
2019-11-16 22:53:23 +0700

    add seeder

--- database/seeds/CommunitiesTableSeeder.php
+++ database/seeds/CommunitiesTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Community;
+use Illuminate\Support\Facades\Schema;
 
 class CommunitiesTableSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class CommunitiesTableSeeder extends Seeder
      */
     public function run()
     {
+        Schema::disableForeignKeyConstraints();
+
+        DB::table('communities')->truncate();
+        $comunities = [
+            ['name' => 'PHP', 'created_at' => date("Y-m-d H:i:s")],
+            ['name' => 'Android', 'created_at' => date("Y-m-d H:i:s")],
+            ['name' => 'Web', 'created_at' => date("Y-m-d H:i:s")],
+            ['name' => 'Database', 'created_at' => date("Y-m-d H:i:s")],
+        ];
+        Community::insert($comunities);
         
+        Schema::enableForeignKeyConstraints();
     }
 }
