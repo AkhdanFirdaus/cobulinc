00402e1e8b117aaced27efbe24290b2877c891f4
Luthfi Sukmawardhani <luthfi552@gmail.com>
2019-11-17 06:45:06 +0700

    Upload image on post

--- resources/views/components/list_post.blade.php
+++ resources/views/components/list_post.blade.php
@@ -1,13 +1,11 @@
 @foreach($posts as $key => $post)
 <div class="card-deck">
     <div class="card my-4">
-        <img src="https://picsum.photos/800/200" class="card-img-top" alt="">
+        <img src="{{ !$post->thumbnail ? 'https://picsum.photos/800/200' : $post->thumbnail }}" class="card-img-top" alt="">
         <div class="card-body">
             <h5 class="card-title">{{ $post->title }}</h5>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, odit quam! Eligendi, non
-                voluptas! Illum, architecto rem! Eveniet tempora obcaecati pariatur. Error fuga porro qui? Assumenda
-                velit ex aut ad.</p>
-            <a href="{{ route('singlepost') }}" class="btn btn-primary">Read More</a>
+            <p>{{ $post->short_description }}</p>
+            <a href="{{ url('post/' . $post->id) }}" class="btn btn-primary">Read More</a>
         </div>
     </div>
 </div>
