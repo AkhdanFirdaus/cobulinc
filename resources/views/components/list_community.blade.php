8f51fa157d53bfef016bdde24962eb424c209c40
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2019-11-17 10:39:55 +0700

    change mylist to mypost

--- resources/views/components/list_community.blade.php
+++ resources/views/components/list_community.blade.php
@@ -1,10 +1,7 @@
 <div class="list-group border-0 shadow-sm">
-  <a href="#" class="list-group-item list-group-item-action active">
-    My List
-  </a>
-  {{-- <a href="{{route('java')}}" class="list-group-item list-group-item-action">Java</a> --}}
-  <a href="#" class="list-group-item list-group-item-action">PHP</a>
-  <a href="#" class="list-group-item list-group-item-action">Android</a>
-  <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Website</a>
+  <a href="#" class="list-group-item list-group-item-action active">Post Saya</a>
+  @foreach(Auth::user()->posts as $key => $userpost)
+    <a href="{{ route('post.show', $userpost->slug) }}" class="list-group-item list-group-item-action">{{ $userpost->title }}</a>
+  @endforeach
 </div>
 
